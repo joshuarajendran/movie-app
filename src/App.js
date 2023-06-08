d0187aa6b18e6e15934981493c99b76f9704d681
Joshua <106053582+thisisanenigma@users.noreply.github.com>
2023-06-08 19:10:03 +0800

    Add popup for movie details

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [favourites, setFavourites] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
+	const apiKey = '62ddf7ce';
 
 	const getMovieRequest = async (searchValue) => {
-		const apiKey = 'YOUR_API_KEY';
-    		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
+    	const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -23,6 +23,15 @@ const App = () => {
 			setMovies(responseJson.Search);
 		}
 	};
+
+	const getMovieDetails = async (movieId) => {
+		const url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apiKey}`;
+	  
+		const response = await fetch(url);
+		const responseJson = await response.json();
+	  
+		return responseJson;
+	  };
 
 	useEffect(() => {
 		getMovieRequest(searchValue);
@@ -60,6 +69,7 @@ const App = () => {
 					movies={movies}
 					handleFavouritesClick={addFavouriteMovie}
 					favouriteComponent={AddFavourites}
+					getMovieDetails={getMovieDetails}
 				/>
 			</div>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
@@ -70,6 +80,7 @@ const App = () => {
 					movies={favourites}
 					handleFavouritesClick={removeFavouriteMovie}
 					favouriteComponent={RemoveFavourites}
+					getMovieDetails={getMovieDetails}
 				/>
 			</div>
 		</div>
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const MovieList = (props) => {
 	const FavouriteComponent = props.favouriteComponent;
-
+  
+	const [hoveredMovies, setHoveredMovies] = useState({});
+  
+	const handleMouseEnter = async (movie) => {
+	  const details = await props.getMovieDetails(movie.imdbID);
+	  setHoveredMovies((prev) => ({ ...prev, [movie.imdbID]: details }));
+	};
+  
+	const handleMouseLeave = (movie) => {
+	  setHoveredMovies((prev) => {
+		const newState = { ...prev };
+		delete newState[movie.imdbID];
+		return newState;
+	  });
+	};
+  
 	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
+	  <>
+		{props.movies.map((movie, index) => (
+		  <div
+			className='image-container d-flex justify-content-start m-3'
+			onMouseEnter={() => handleMouseEnter(movie)}
+			onMouseLeave={() => handleMouseLeave(movie)}
+		  >
+			<img src={movie.Poster} alt='movie'></img>
+			<div
+			  onClick={() => props.handleFavouritesClick(movie)}
+			  className='overlay d-flex align-items-center justify-content-center'
+			>
+			  <FavouriteComponent />
+			</div>
+			{hoveredMovies[movie.imdbID] && (
+			  <div className='overlay-details'>
+				<h3>{hoveredMovies[movie.imdbID].Title}</h3>
+				<p>{hoveredMovies[movie.imdbID].Year}</p>
+				<p>{hoveredMovies[movie.imdbID].Plot}</p>
+			  </div>
+			)}
+		  </div>
+		))}
+	  </>
 	);
-};
-
-export default MovieList;
+  };
+  
+  export default MovieList;
+  
